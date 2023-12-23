b1a56199e3f8d7b718c5d53c2c83662c9e1cbaa0
Lamine DIOUBATE <domanilamine96@gmail.com>
2023-12-23 15:48:21 -0500

    temperature converter pratique debut

--- App.js
+++ App.js
@@ -1,20 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ImageBackground, Text, View } from "react-native";
+import coldImg from "./assets/cold.png";
+import hotImg from "./assets/hot.png";
+
+import { s } from "./app.style";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ImageBackground source={coldImg} style={s.container}>
+      <View>
+        <Text>Lamine</Text>
+      </View>
+    </ImageBackground>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
